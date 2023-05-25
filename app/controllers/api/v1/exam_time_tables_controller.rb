0f7b0cd735edfe9550bbb3dd60fc64c99882bf0a
nirmal-vaja <nirmalvaja123@gmail.com>
2023-05-25 14:58:00 +0530

    Fix

--- app/controllers/api/v1/exam_time_tables_controller.rb
+++ app/controllers/api/v1/exam_time_tables_controller.rb
@@ -19,91 +19,6 @@ module Api
         }
       end
 
-      def get_examination_dates
-        if @exam_time_tables
-          exam_time_tables = @exam_time_tables.map do |time_table|
-            time_table.attributes.merge(
-              {
-                subject_name: time_table.subject.name,
-                subject_code: time_table.subject.code
-              }
-            )
-          end
-
-          render json: {
-            message: "Examination dates are as below",
-            data: {
-              dates: exam_time_tables,
-            },status: :ok
-          }
-        else
-          render json: {
-            message: "No timetable found",
-            status: :unprocessable_entity
-          }
-        end
-      end
-
-      def fetch_details
-        subject = Subject.find_by_id(params[:id])
-        @exam_time_table = ExamTimeTable.find_by(subject_id: subject.id) if subject.present?
-        exam_time_table = @exam_time_table.attributes.merge(
-          {
-            subject_name: subject.name,
-            subject_code: subject.code
-          }
-        ) if @exam_time_table.present?
-        
-        if exam_time_table
-          render json: {
-            message: "Details found",
-            data: {
-              time_table: exam_time_table
-            }, status: :ok
-          }
-        else
-          render json: {
-            message: "No timetable found for #{subject.name}, create one!",
-            status: :unprocessable_entity
-          }
-        end
-      end
-
-      def fetch_subjects
-        @exam_time_table = ExamTimeTable.joins(:subject).where(academic_year: params[:id]).or(ExamTimeTable.joins(:subject).where(name: params[:examination_name]))
-
-        if @exam_time_table
-          render json: {
-            message: "These are the time table objects",
-            data: {
-              subject_info: @exam_time_table.pluck(:code, 'subjects.name')
-            }, status: :ok
-          }
-        else
-          render json: {
-            message: "No time table found",
-            status: :unprocessable_entity
-          }
-        end
-      end
-      
-      def fetch_dates
-        @exam_time_tables = ExamTimeTable.where(academic_year: params[:id])
-        if @exam_time_tables
-          render json: {
-            message: "These are the time table objects",
-            data: {
-              dates: @exam_time_tables.pluck(:date)
-            }, status: :ok
-          }
-        else
-          render json: {
-            message: "No timetable found for #{academic_year} year",
-            status: :unprocessable_entity
-          }
-        end
-      end
-
       def create
         @exam_time_table = ExamTimeTable.new(time_table_params)
         @exam_time_table.semester = @exam_time_table.subject.semester
@@ -151,6 +66,47 @@ module Api
         else
           render json: {
             message: @time_table.errors.full_messages.join(' '),
+            status: :unprocessable_entity
+          }
+        end
+      end
+
+      def get_examination_dates
+        if @exam_time_tables
+          render json: {
+            message: "Examination dates are as below",
+            data: {
+              dates: @exam_time_tables.pluck(:date).uniq,
+            },status: :ok
+          }
+        else
+          render json: {
+            message: "No timetable found",
+            status: :unprocessable_entity
+          }
+        end
+      end
+
+      def fetch_details
+        subject = Subject.find_by_id(params[:id])
+        @exam_time_table = ExamTimeTable.find_by(subject_id: subject.id) if subject.present?
+        exam_time_table = @exam_time_table.attributes.merge(
+          {
+            subject_name: subject.name,
+            subject_code: subject.code
+          }
+        ) if @exam_time_table.present?
+        
+        if exam_time_table
+          render json: {
+            message: "Details found",
+            data: {
+              time_table: exam_time_table
+            }, status: :ok
+          }
+        else
+          render json: {
+            message: "No timetable found for #{subject.name}, create one!",
             status: :unprocessable_entity
           }
         end
--- app/controllers/api/v1/time_table_block_wise_reports_controller.rb
+++ app/controllers/api/v1/time_table_block_wise_reports_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class TimeTableBlockWiseReportsController < ApiController
 
+      before_action :set_report, 
+
       def index
         @reports = TimeTableBlockWiseReport.where(report_params)
 
@@ -66,7 +68,31 @@ module Api
         end
       end
 
+      def update
+        equation = @report.no_of_students.to_f / 30
+        @report.rooms = equation.ceil()
+        @report.blocks = equation.ceil()
+
+        if @report.save
+          render json: {
+            message: "Updated!",
+            data: {
+              report: @report,
+            }, status: :ok
+          }
+        else
+          render json: {
+            message: @report.errors.full_messages.join(' '),
+            status: :unprocessable_entity
+          }
+        end
+      end
+
       private
+
+      def set_report
+        @report = TimeTableBlockWiseReport.find_by_id(params[:id])
+      end
 
       def report_params
         params.require(:report).permit(:academic_year, :no_of_students, :exam_time_table_id, :examination_name, :course_id, :branch_id, :semester_id)

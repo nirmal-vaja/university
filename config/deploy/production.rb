3c6cb713199b0e16bedeaee041c0755bad597ddb
nirmal-vaja <nirmalvaja123@gmail.com>
2023-07-06 12:08:44 +0530

    Fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,5 +64,5 @@ set :smtp_address, 'smtp.gmail.com'
 set :smtp_port, 587
 set :smtp_user_name, ENV['GMAIL_USERNAME']
 set :smtp_password, ENV['GMAIL_PASSWORD']
-set :smtp_authentication, :plain
+set :smtp_authentication, :login
 set :smtp_enable_starttls_auto, true

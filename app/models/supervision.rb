92f503b7a5de5a5a78d2e8512fd8dcb2beec3ce7
nirmal-vaja <nirmalvaja123@gmail.com>
2023-07-11 15:23:11 +0530

    Fix

--- app/models/supervision.rb
+++ app/models/supervision.rb
@@ -7,9 +7,4 @@ class Supervision < ApplicationRecord
   attr_accessor :date
 
   # validates :user_id, uniqueness: { scope: [:list_type, :time] }
-
-  enum time: {
-    "10:30 A.M to 01:00 P.M": 0 ,
-    "03:00 P.M to 05:30 P.M": 1
-  }
 end

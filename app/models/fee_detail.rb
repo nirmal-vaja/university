e22639adbf9ea593dd10af81ee9e0e8f8f922943
nirmal-vaja <nirmalvaja123@gmail.com>
2023-07-26 16:56:23 +0530

    Fix

--- app/models/fee_detail.rb
+++ app/models/fee_detail.rb
@@ -4,4 +4,12 @@ class FeeDetail < ApplicationRecord
   belongs_to :semester
 
   validates :academic_year, presence: true
+
+  def as_json(options = {})
+    super(options).merge(
+      course: course,
+      branch: branch,
+      semester: semester
+    )
+  end
 end

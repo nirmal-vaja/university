14ce5494677260ed9f4301788a8c79e43fd79c1a
nirmal-vaja <nirmalvaja123@gmail.com>
2023-07-31 10:57:16 +0530

    Fix

--- config/initializers/razorpay.rb
+++ config/initializers/razorpay.rb
@@ -2,4 +2,6 @@ require 'razorpay/order'
 require 'razorpay/customer'
 key_id = ENV["RAZORPAY_KEY_ID"]
 secret_key = ENV["RAZORPAY_SECRET_KEY"]
+Rails.logger.info "RAZORPAY_KEY_ID: #{ENV['RAZORPAY_KEY_ID']}"
+Rails.logger.info "RAZORPAY_SECRET_KEY: #{ENV['RAZORPAY_SECRET_KEY']}"
 Razorpay.setup(key_id, secret_key)

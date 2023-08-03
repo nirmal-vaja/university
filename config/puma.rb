da1a89ace1c6296fa5c2460c0c94106ac1c4a82a
niravdarji123 <darjiniravv19@gmail.com>
2023-08-03 15:18:18 +0530

    Fix

--- config/puma.rb
+++ config/puma.rb
@@ -24,6 +24,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+bind "unix:///home/app/university/shared/tmp/sockets/university-puma.sock"
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.

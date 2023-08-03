fba3f5ad4a36b7583311b985b51509c8d80a9e85
niravdarji123 <darjiniravv19@gmail.com>
2023-08-03 15:22:17 +0530

    Fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,11 +17,11 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
-set :puma_user, fetch(:user)
-set :puma_enabled_socket_service, true
-set :puma_service_unit_env_files, []
-set :puma_service_unit_env_vars, []
-set :puma_role, :web
+# set :puma_user, fetch(:user)
+# set :puma_enabled_socket_service, true
+# set :puma_service_unit_env_files, []
+# set :puma_service_unit_env_vars, []
+# set :puma_role, :web
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production

21a2ca69087479df1bc3877703e2b51600231b00
nirmal-vaja <nirmalvaja123@gmail.com>
2023-08-04 16:45:36 +0530

    Fix

---
+++ app/controllers/api/v1/student_certificates_controller.rb
@@ -0,0 +1,49 @@
+module Api
+  module V1
+    class StudentCertificatesController < ApiController
+
+      def index
+        @student_certificates = StudentCertificate.all
+
+        if @student_certificates
+          render json: {
+            message: "Details found",
+            data: {
+              student_certificates: @student_certificates
+            }, status: :ok
+          }
+        else
+          render json: {
+            message: "Details not found",
+            status: :not_found
+          }
+        end
+      end
+
+      def update
+        @student_certificate = StudentCertificate.find_by_id(params[:id])
+
+        if @student_certificate.update(student_certificate_params)
+          event = @student_certificate.status === "accept" ? "accepted" : "rejected"
+          render json: {
+            message: "Request #{event}",
+            data: {
+              student_certificate: @student_certificate
+            },status: :ok
+          }
+        else
+          render json: {
+            message: @student_certificate.errors.full_messages.join(", "),
+            status: :unprocessable_entity
+          }
+        end
+      end
+
+      private
+
+      def student_certificate_params
+        params.require(:student_certificate).permit(:status, :notes)
+      end
+    end
+  end
+end

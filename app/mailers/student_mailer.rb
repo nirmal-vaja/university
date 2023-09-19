5ded8cba52a95bd3a97a7ae6d97b21776a13bf8c
nirmal-vaja <nirmalvaja123@gmail.com>
2023-09-19 17:33:17 +0530

    Fix

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -1,6 +1,6 @@
 class StudentMailer < ApplicationMailer
   def send_otp_mail(student)
     @student = student
-    mail(to: @student.contact_details.personal_email_address, subject: 'You received an OTP')
+    mail(to: @student.contact_detail.personal_email_address, subject: 'You received an OTP')
   end
 end
